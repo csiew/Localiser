adc75c36928d7816e4714c698c30414716976d8c
csiew <clarence.siew@gmail.com>
2020-04-02 10:15:59 +1100

    Roles for accounts in accounts DB schema

--- database/account_db_schema.sql
+++ database/account_db_schema.sql
@@ -1,8 +1,9 @@
 CREATE TABLE roles(
 	role_id			VARCHAR(64)		PRIMARY KEY
-)
+);
 
 CREATE TABLE account(
 	username		VARCHAR(128)		PRIMARY KEY,
+	role_id			VARCHAR(64)			REFERENCES roles(role_id),
 	password		VARCHAR(64)			NOT NULL
 );
